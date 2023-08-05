9a296109ec83f0fe620d2c0e9fce8eca510b3cbf
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-08-05 12:18:26 -0500

    Convert type gameData into Class

---
+++ src/constructors/gameData.ts
@@ -0,0 +1,41 @@
+import GameBoard from "./board";
+import { Player } from "./player";
+import Ship from "./ship";
+
+// Ships should be at maximum the half of board
+const BOARD_SIZE = 10;
+const SHIPS_AMMOUNT = 5;
+const SHIP_SIZES = [4, 4, 3, 3, 2];
+
+// TODO: Possibility to open game for 2 players in future
+
+export class GameData {
+  player: Player;
+  computer: Player;
+  isGameOver: boolean;
+  playerBoard: GameBoard;
+  computerBoard: GameBoard;
+  playerShips: Ship[];
+  computerShips: Ship[];
+
+  constructor(playerName: string) {
+    this.player = new Player(playerName);
+    this.computer = new Player("computer");
+    this.playerBoard = new GameBoard(BOARD_SIZE);
+    this.computerBoard = new GameBoard(BOARD_SIZE);
+    this.playerShips = this.generateShips(SHIPS_AMMOUNT);
+    this.computerShips = this.generateShips(SHIPS_AMMOUNT);
+    this.isGameOver = false;
+  }
+
+  // Returns an array of Ships
+  // The sizes are defined above
+  private generateShips(ammount: number) {
+    const ships = [];
+    for (let i = 0; i < ammount; i++) {
+      ships.push(new Ship(SHIP_SIZES[i]));
+    }
+
+    return ships;
+  }
+}
--- src/types/type.ts
+++ src/types/type.ts
@@ -11,10 +11,3 @@ export type ShipPosition = {
   start: Coords;
   ship: Ship;
 };
-
-// General Types
-
-export type gameData = {
-  player: Player;
-  computer: Player;
-};
