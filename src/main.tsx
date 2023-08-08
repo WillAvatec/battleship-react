c959c0849c5a5fd1a267fabec1d66f2988777782
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-08-07 19:52:44 -0500

    feat: new router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import PlacementMenu from "./tabs/PlacementMenu.tsx";
+import MainGame from "./tabs/MainGame.tsx";
 import App from "./App.tsx";
-import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { GameDataProvider } from "./context/data.tsx";
+import "./index.css";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/placement", element: <PlacementMenu /> },
+  { path: "/game", element: <MainGame /> },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GameDataProvider>
-      <App />
+      <RouterProvider router={router} />
     </GameDataProvider>
   </React.StrictMode>
 );
