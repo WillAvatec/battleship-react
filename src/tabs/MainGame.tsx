6b0f6372dde7d0c5170b95677be0e51c178938c5
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-08-11 17:38:28 -0500

    feat: new MainGame tab

--- src/tabs/MainGame.tsx
+++ src/tabs/MainGame.tsx
@@ -1,39 +1,94 @@
-import Board from "../components/Board";
-import GameBoard from "../constructors/board";
-import { memBoardType } from "../types/type";
+import { useContext, useEffect, useState } from "react";
+import { DataContext } from "../context/data";
+import { Coords, HitCell } from "../types/type";
+import PlayerBoard from "../components/MainBoard";
+import { useNavigate } from "react-router-dom";
 
 function MainGame() {
-  const player_fake = new GameBoard(10);
-  const ai_fake = new GameBoard(10);
+  // Get data from context
+  const game = useContext(DataContext);
+  const {
+    playerBoard,
+    player,
+    computerBoard,
+    computer,
+    updateBoard,
+    gameState,
+    setWinner,
+  } = game;
+
+  const [pcBoardCells, setPcBoardCells] = useState<HitCell[]>([]);
+  const [playerBoardCells, setPlayerBoardCells] = useState<HitCell[]>([]);
+
+  const goTo = useNavigate();
+
+  useEffect(() => {
+    if (gameState !== "onPlay") goTo("/");
+  });
+
+  //Player always starts the game
+  const handlePlayerAttack = (e: React.MouseEvent) => {
+    const target = e.target as HTMLButtonElement;
+    const coords: Coords = {
+      column: Number(target.dataset.column),
+      row: Number(target.dataset.row),
+    };
+    //Try to attack computerBoard
+    let didHit: boolean;
+    try {
+      didHit = computerBoard.receiveAttack(coords);
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+
+    //Check winner
+    if (computerBoard.areAllSunked()) {
+      setWinner(player.name);
+      goTo("/winner");
+    }
+
+    updateBoard(computerBoard);
+    setPcBoardCells((prevArr) => [...prevArr, { coords, landed: didHit }]);
+
+    // Now hand to the pc
+    handlePCAttack();
+  };
+
+  const handlePCAttack = () => {
+    const randomAttack = computer.randomAttack(playerBoard);
+    if (randomAttack) {
+      const { coords, didHit } = randomAttack;
+      if (playerBoard.areAllSunked()) {
+        setWinner(computer.name);
+        goTo("/lose");
+      }
+      setPlayerBoardCells((prevArr) => [
+        ...prevArr,
+        { coords, landed: didHit },
+      ]);
+    }
+  };
 
   return (
     <div
-      id="main"
+      id="main-game"
       className="wrapper mx-auto flex justify-center items-center flex-1 min-h-full"
     >
       <section className="boards flex flex-col gap-5 lg:gap-20 md:flex-row">
-        <PlayerBoard memBoard={player_fake.memBoard} name="Emanuel" />
-        <PlayerBoard memBoard={ai_fake.memBoard} name="Wilson!" />
+        <PlayerBoard
+          memBoard={playerBoard.memBoard}
+          attackedCells={playerBoardCells}
+          name={player.name}
+          onClick={() => {}}
+        />
+        <PlayerBoard
+          memBoard={computerBoard.memBoard}
+          attackedCells={pcBoardCells}
+          onClick={handlePlayerAttack}
+          name={computer.name}
+        />
       </section>
-    </div>
-  );
-}
-
-function PlayerBoard({
-  memBoard,
-  name,
-}: {
-  memBoard: memBoardType;
-  name: string;
-}) {
-  return (
-    <div className="player-side p-5 relative">
-      <div className="bg-pink-600 -z-10 absolute inset-0 blur" />
-      <div className="player-name overflow-hidden whitespace-nowrap text-violet-50 max-w-sm text-ellipsis drop-shadow-2xl inline-block text-xl absolute -top-4 sm:-top-8">
-        {name}
-      </div>
-
-      <Board memBoard={memBoard} />
     </div>
   );
 }
