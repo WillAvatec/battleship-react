91d005aa8794037c8f76ebd2306a7b16f2f0c3f9
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-08-11 17:42:57 -0500

    State is now in the context instead of GameState

--- src/context/data.tsx
+++ src/context/data.tsx
@@ -1,40 +1,82 @@
 import { createContext, useState } from "react";
-import { GameData } from "../constructors/gameData";
-import { GameDataContextValue } from "../types/type";
+import { GameDataContextValue, gameState } from "../types/type";
+import GameBoard from "../constructors/board";
+import Ship from "../constructors/ship";
+import { Player } from "../constructors/player";
 
-/*
-    The context should be initialized to null, only after, 
-    getting the player's name; it should be initialize a new GameData 
-*/
+const BOARD_SIZE = 10;
+const SHIPS_AMMOUNT = 5;
+const SHIP_SIZES = [4, 4, 3, 3, 2];
 
 // Create context
-export const DataContext = createContext<GameDataContextValue>({
-  data: new GameData(),
-  initGameData: () => {},
-  winner: "",
-});
+export const DataContext = createContext<GameDataContextValue>(
+  {} as GameDataContextValue
+);
 
 // Create provider (it's wrapped inside GameDataProvider)
 export function GameDataProvider({ children }: { children: React.ReactNode }) {
-  const [gameData, setGameData] = useState<GameData>(new GameData());
+  const [player, setPlayer] = useState<Player>(new Player());
+  const [playerBoard, setPlayerBoard] = useState(new GameBoard(10));
+  const [playerShips, setPlayerShips] = useState<Ship[]>([]);
+  const [computer, setComputer] = useState<Player>(new Player("computer"));
+  const [computerBoard, setComputerBoard] = useState(new GameBoard(10));
+  const [computerShips, setComputerShips] = useState<Ship[]>([]);
+  const [gameState, setGameState] = useState<gameState>("start");
   const [winner, setWinner] = useState("");
 
   const initGameData = (name?: string) => {
-    setGameData(() => {
-      // Change name and set gameState to onPlay
-      const newData = new GameData(name);
-      newData.gameState = "onPlay";
-      return newData;
+    //PC DATA
+    const newShips = generateShips(SHIPS_AMMOUNT);
+    setComputer(new Player("computer"));
+    setComputerShips(newShips);
+    setComputerBoard(() => {
+      const newBoard = new GameBoard(BOARD_SIZE);
+      newBoard.generateRandomBoard(newShips);
+      return newBoard;
     });
+
+    //Player DATA
+    setPlayer(new Player(name));
+    setPlayerShips(generateShips(SHIPS_AMMOUNT));
+    setPlayerBoard(new GameBoard(BOARD_SIZE));
+
+    //General DATA
+    setGameState("setup");
     setWinner("");
+  };
+
+  const updateState = (state: gameState) => {
+    setGameState(state);
+  };
+
+  const generateShips = (ammount: number) => {
+    const ships = [];
+    for (let i = 0; i < ammount; i++) {
+      ships.push(new Ship(SHIP_SIZES[i]));
+    }
+
+    return ships;
+  };
+
+  const updateBoard = (board: GameBoard) => {
+    setComputerBoard(() => board);
   };
 
   return (
     <DataContext.Provider
       value={{
-        data: gameData,
+        computerBoard,
+        computerShips,
+        playerBoard,
+        playerShips,
+        updateState,
+        updateBoard,
         initGameData,
+        gameState,
+        computer,
+        player,
         winner,
+        setWinner,
       }}
     >
       {children}
