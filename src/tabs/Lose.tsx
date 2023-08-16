33059494e12d508d64d23c9306d167a45cf67041
Lizbeth-Laptop <115133403+WillAvatec@users.noreply.github.com>
2023-08-15 22:31:38 -0500

    fix: Unwrapping images removed

--- src/tabs/Lose.tsx
+++ src/tabs/Lose.tsx
@@ -12,16 +12,12 @@ function LosePage() {
   const { winner } = gameData;
 
   return (
-    <div className="fixed z-10 left-0 top-0 w-full h-full bg-no-repeat bg-cover min-w-full min-h-full flex flex-col justify-center content-center">
-      <img
-        className="w-72 object-contain self-center translate-y-20"
-        src="https://media.makeameme.org/created/dont-give-up-5b2cb3.jpg"
-      />
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,400px))] justify-center gap-7 min-h-[150px] p-5 w-11/12 mx-auto my-auto justify-items-center rounded-xl bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white max-w-screen-sm">
+    <div className="fixed z-10 left-0 top-0 w-full h-full bg-no-repeat bg-black bg-opacity-50 bg-cover min-w-full min-h-full flex flex-col justify-center content-center">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,400px))] justify-center gap-7 min-h-[150px] p-5 w-11/12 mx-auto my-auto justify-items-center rounded-xl bg-gradient-to-r from-amber-500 to-amber-700 text-white max-w-screen-sm">
         <div className="flex flex-col align-center">
           <h1
             id="congratulations"
-            className="text-2xl text-center align-middle leading-8 h-9"
+            className="text-2xl text-center align-middle leading-8 h-2"
           >
             Oops, the winner this time was
           </h1>
@@ -35,7 +31,7 @@ function LosePage() {
         <button
           id="newGame"
           onClick={handleClick}
-          className="text-white bg-purple-800 px-6 py-3 rounded-xl hover:scale-105 hover:bg-purple-900 hover:shadow-xl transition-all"
+          className="text-white bg-green-500 px-6 py-3 rounded-xl hover:scale-105 hover:bg-green-700 hover:shadow-xl transition-all"
         >
           Try Again
         </button>
--- src/tabs/Victory.tsx
+++ src/tabs/Victory.tsx
@@ -12,12 +12,9 @@ function VictoryPage() {
   const { winner } = gameData;
 
   return (
-    <div className="fixed z-10 left-0 top-0 w-full h-full bg-no-repeat bg-cover min-w-full min-h-full flex flex-col justify-center content-center">
-      <img
-        className="w-52 object-contain self-center translate-y-32"
-        src="https://img.freepik.com/free-vector/vintage-sport-prize-template_1284-40599.jpg?w=2000"
-      />
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,400px))] justify-center gap-7 min-h-[150px] p-5 w-11/12 mx-auto my-auto justify-items-center rounded-xl bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white max-w-screen-sm">
+    <div className="fixed z-10 left-0 top-0 w-full h-full bg-no-repeat bg-cover bg-black bg-opacity-50 min-w-full min-h-full flex flex-col justify-center content-center">
+      
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,400px))] justify-center gap-7 min-h-[150px] p-5 w-11/12 mx-auto my-auto justify-items-center rounded-xl bg-gradient-to-r from-amber-500 to-amber-800 text-white max-w-screen-sm">
         <div className="flex flex-col align-center">
           <h1
             id="congratulations"
@@ -33,7 +30,7 @@ function VictoryPage() {
         <button
           id="newGame"
           onClick={handleClick}
-          className="text-white bg-purple-800 px-6 py-3 rounded-xl hover:scale-105 hover:bg-purple-900 hover:shadow-xl transition-all"
+          className="text-white bg-green-500 px-6 py-3 rounded-xl hover:scale-105 hover:bg-green-700 hover:shadow-xl transition-all"
         >
           Play Again
         </button>
