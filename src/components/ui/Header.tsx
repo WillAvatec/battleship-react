ec7f52aa6d13bedfae5ca3a449bbe870d731fa11
Lizbeth-Laptop <115133403+WillAvatec@users.noreply.github.com>
2023-08-15 23:00:11 -0500

    style: Change background color

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,13 +2,14 @@ import Helmet from "../../assets/helmet.png"
 
 function Header(){
     return (
-        <header className="bg-amber-100 relative py-4 text-center text-green-950 text-3xl font-bold tracking-wider flex flex-row justify-center items-center gap-4">
-            <nav className="absolute left-0 sm:left-5 bottom-0 flex flex-row gap-1 items-center">
+        <header className="bg-black relative py-4 text-center text-white text-3xl font-bold tracking-wider flex flex-row justify-center items-center gap-4">
+            <nav className="absolute left-1 sm:left-5 bottom-2 flex flex-row gap-1 items-center">
                 <div id="left-triangle" />
-                <a className="text-base tracking-normal underline" href="/">Return</a>
+                <a className="text-sm font-normal  sm:text-base tracking-normal underline" href="/">Return</a>
             </nav>
             <img className="hidden sm:block w-10" src={Helmet} />
             <p>BATTLESHIP</p>
+            <img className="hidden sm:block w-10" src={Helmet} />
         </header>
     )
 }
